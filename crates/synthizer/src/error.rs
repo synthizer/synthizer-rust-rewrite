6bbf9af5602f13eebcff86a9feef9344764405b1
Austin Hicks <ahicks@ahicks.io>
2024-01-07 16:06:01 -0800

    Hook LoopSpec related errors into the er

--- crates/synthizer/src/error.rs
+++ crates/synthizer/src/error.rs
@@ -1,14 +1,18 @@
 use synthizer_miniaudio::Error as MiniaudioError;
 
+use crate::loop_spec::LoopSpecError;
 use crate::sample_sources::SampleSourceError;
 
-#[derive(Debug, derive_more::Display)]
+#[derive(Debug, derive_more::Display, derive_more::IsVariant)]
 enum ErrorPayload {
     #[display(fmt = "Audio backend error: {}", _0)]
     AudioBackend(MiniaudioError),
 
     #[display(fmt = "Sample source error: {}", _0)]
     SampleSource(SampleSourceError),
+
+    #[display(fmt = "Loop specification error: {}", _0)]
+    LoopSpec(crate::loop_spec::LoopSpecError),
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -31,5 +35,13 @@ macro_rules! conv {
 
 conv!(AudioBackend, MiniaudioError);
 conv!(SampleSource, SampleSourceError);
+conv!(LoopSpec, LoopSpecError);
+
+impl Error {
+    /// Does this error represent an invalid [crate::LoopSpec]?
+    pub fn is_invalid_loop(&self) -> bool {
+        self.payload.is_loop_spec()
+    }
+}
 
 pub type Result<T, E = Error> = std::result::Result<T, E>;
